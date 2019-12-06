3ff9b2682fc3b97c38b93901d0a217f167d3fe0e
RubanSA <ruban_sa@hotmail.com>
2019-12-06 02:10:12 +0200

    Добавлены интерфейс и реализация репозитория

---
+++ ASP_Lab3/ASP_Lab3/Models/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASP_Lab3.Models
+{
+    public interface IRepository
+    {
+        void Add(string str);
+        void Remove(string str);
+        void RemoveAt(int index);
+        void Clear();
+    }
+}
---
+++ ASP_Lab3/ASP_Lab3/Models/StringRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP_Lab3.Models
+{
+    public class StringRepository : IRepository
+    {
+        private List<string> list = new List<string>();
+        public void Add(string str)
+        {
+            list.Add(str);
+        }
+
+        public void Clear()
+        {
+            list.Clear();
+        }
+
+        public void Remove(string str)
+        {
+            list.Remove(str);
+        }
+
+        public void RemoveAt(int index)
+        {
+            list.RemoveAt(index);
+        }
+    }
+}
